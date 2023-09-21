918e3ceca3f9b9c8ddc60b55439f10ce2a1dd2e9
Iheanyi <ionawsinfra@gmail.com>
2023-09-21 11:53:33 -0400

    recreate via pipeline

--- eks/eks_stack.py
+++ eks/eks_stack.py
@@ -13,62 +13,52 @@ class EksStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
         # Create a VPC with default configuration
-        vpc = ec2.Vpc(self, 'MyEKSVPC'
-                      )
-        
-        
+        vpc = ec2.Vpc(self, 'MyEKSVPC')
+
         # Creates an ECR Repository
         repo = ecr.Repository(self, 'MyECRRepo',
-                              repository_name = 'dbecrrepo')
-        
-        
+                              repository_name='dbecrrepo')
+
         # Creates an IAM role for EKS Cluster
         cluster_role = iam.Role(self, 'ClusterAdminRole',
                                 assumed_by=iam.ServicePrincipal('eks.amazonaws.com'),
                                 managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name('AmazonEKSClusterPolicy')]
-                            )
-        
-        # Creates an EKS Cluster
+                                )
+
+        # Create an IAM role for system:masters
+        masters_role = iam.Role(self, "MastersRole",
+                                assumed_by=iam.ServicePrincipal("sts.amazonaws.com"))
+        masters_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEKSClusterPolicy"))
+        masters_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEKSWorkerNodePolicy"))
+
+        # Creates an EKS Cluster with masters_role
         cluster = eks.Cluster(self, 'MyCluster',
                               vpc=vpc,
                               default_capacity=0,
                               version=eks.KubernetesVersion.V1_25,
                               role=cluster_role,
+                              masters_role=masters_role,  # Associate the masters role
                               cluster_name='dbekscluster'
                               )
-        
-        # Cretaes a linux node group with security configurations
+
+        # Creates a linux node group with security configurations
         nodegroup = cluster.add_nodegroup_capacity('MyNodeGroup',
-                                                   nodegroup_name= 'mynodegroup',
-                                                   instance_types=[ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO)], 
-                                                   min_size = 2,
-                                                   max_size = 5,
-                                                   desired_size = 3,
-                                                   subnets = ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
-                                            )
-        
-        # # Defines security group for EKS node group
-        # node_sg = ec2.SecurityGroup(self, 'NodeSecGroup',
-        #                             vpc=vpc,
-        #                             allow_all_outbound= True) #Set to false to restrict outbound traffic
-        
-        # # Allows specific inbound traffic. For eg, allow traffic on port 80:
-        # node_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), 'Allow HTTP Traffic')
-        
-        # # Associate security group with Node group
-        # nodegroup.add_
-        
+                                                   nodegroup_name='mynodegroup',
+                                                   instance_types=[ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO)],
+                                                   min_size=2,
+                                                   max_size=5,
+                                                   desired_size=3,
+                                                   subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
+                                                   )
+
         # Define a VPC Endpoint for S3 (as an example)
         s3_endpoint = vpc.add_gateway_endpoint('S3Endpoint',
-                                              service=ec2.GatewayVpcEndpointAwsService.S3, 
-                                              subnets=[ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)]
-                                              )
+                                               service=ec2.GatewayVpcEndpointAwsService.S3,
+                                               subnets=[ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)]
+                                               )
+
         # Apply Removal Policy
-        resources = [ vpc, nodegroup, repo, cluster_role, s3_endpoint]
+        resources = [vpc, nodegroup, repo, cluster_role, s3_endpoint]
         for resource in resources:
-                resource.apply_removal_policy(RemovalPolicy.DESTROY)
-        
-        
-        
+            resource.apply_removal_policy(RemovalPolicy.DESTROY)
